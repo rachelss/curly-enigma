77af26620f5ddb430dd3c103cacea8da12ea929b
Rachel Schwartz <rsschwartz@gmail.com>
2017-02-14 13:42:10 -0500

    initial commit

---
+++ 01-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+ echo "Usage: $0 filename"
+ exit 1
+fi
+
+filename=$1
+
+echo -n "The number of genes in $filename is "
+
+cat $filename |
+ grep -v '^#' |
+ cut -f3 |
+ grep gene |
+ wc -l
